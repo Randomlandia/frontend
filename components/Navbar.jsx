0247acfe89ca852aa9368a5255b2e0cbc8a047a5
angelagar <ggarcia.angelad@gmail.com>
2024-06-15 13:23:38 -0600

    responsive navbar, fonts added, app configured for using google fonts api

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,25 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className='w-full h-16 bg-[#FF9D30] p-'></nav>
+      <nav className='w-full h-14 bg-[#FF9D30]  flex justify-between text-white font-lucky text-xl'>
+        <div>logo</div>
+        <div className='flex'>
+          <div className='hidden lg:flex gap-7 items-center px-3'>
+            <button>NOSOTROS</button>
+            <button>INICIAR SESIÓN</button>
+            <button className='bg-[#E56C25] h-9 px-5 rounded-[10px]'>
+              CREAR CUENTA
+            </button>
+            <button className='bg-[#44AA68] h-9 px-5 rounded-[10px]'>
+              ¡JUGAR!
+            </button>
+          </div>
+          <div className='flex lg:hidden items-center gap-2'>
+            <button className='text-sm leading-4 bg-[#E56C25] px-6 py-2 rounded-[10px]'>INICIAR<br/>SESIÓN</button>
+            <img src='/menu.svg' alt='menu' className='w-14 h-16'/>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mont': ['"Montserrat Alternates"', 'sans-serif'],
+        'lucky': ['"Luckiest Guy"', 'cursive'],
+        'ram': ['"Rammetto One"', 'cursive'],
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: []
+}
