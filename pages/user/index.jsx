251f543ad580e122cadc6575d7060ad76a6d53ea
Sonia Aguirre <arembiam@gmail.com>
2024-06-17 20:22:25 -0600

    First Soni commit

---
+++ pages/user/index.jsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import Avatar from "@/components/Avatar";
+
+export default function User() {
+  return (
+    <main className="w-full min-h-screen bg-white">
+      {/* NAVBAR COMPONENT */}
+      <Navbar />
+      {/* BODY */}
+      <div className="flex m-6">
+        <div className="bg-yellow-200 rounded-xl p-4 w-full flex-row">
+          {/* AVATAR COMPONENT Y NOMBRE USER*/}
+          <Avatar />
+          <div className="bg-orange-500 rounded-lg text-center p-2">
+            JuanElChato92
+          </div>
+          {/* SCORE */}
+          {/* BOTONES HACIA SUBMENUS */}
+        </div>
+      </div>
+
+      {/* FOOTER CONTACTO */}
+    </main>
+  );
+}
