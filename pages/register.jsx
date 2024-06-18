ed3bf47c392f67333db650dbf5bb2907317f2352
angelagar <ggarcia.angelad@gmail.com>
2024-06-18 00:32:49 -0600

    last on register

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,5 +1,5 @@
-import Navbar from "@/components/Navbar"
-import Link from "next/link"
+import Navbar from '@/components/Navbar'
+import Link from 'next/link'
 
 export default function Register() {
   return (
@@ -9,21 +9,24 @@ export default function Register() {
         <div className='grid justify-items-center bg-[#d9d9d930] h-4/5 w-[350px] md:w-4/5 lg:w-1/2 py-24 px-8 mx-auto rounded-xl'>
           <form
             action='submit'
-            className='w-full md:w-[424px] pt-3 grid gap-7 text-sm font-bold'
+            className='w-80 md:w-[424px] pt-3 grid gap-7 text-sm font-bold'
           >
             <div className='grid gap-2'>
               <div className='grid gap-0.5'>
-                <label name='email' className='px-2 '>
+                <label name='email' className='px-14 '>
                   EMAIL{' '}
                 </label>
-                <div className="flex gap-2 font-bold">
-                  <img src="/mail.svg" alt="" className="w-9 h-9"/>
+                <div className='flex gap-2 font-bold'>
+                  <img src='/mail.svg' alt='' className='w-10 h-10' />
+                  {/* <span class='material-symbols-outlined'>mail</span>
+                  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0" className=''/> */}
+
 
                   <input
                     type='email'
                     name='email'
                     placeholder='ejemplo@email.com'
-                    className='bg-lorange/50 text-white p-2 rounded-lg'
+                    className='bg-lorange/80 text-white w-56 p-2 rounded-lg'
                   />
                 </div>
               </div>
