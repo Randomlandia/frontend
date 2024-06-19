869aa7386e7e7308c39b0ff6d91ff37d43f0cee3
Sonia Aguirre <148831155+arembiam@users.noreply.github.com>
2024-06-18 23:42:09 -0600

    feat: footer and incomplete user profile
    * First Soni commit
    
    * new?
    
    * contacto footer estructura
    
    * contacto footer update, falta randy
    
    * contacto footer component done
    
    * estructura user menu

---
+++ components/Avatar.jsx
@@ -0,0 +1,5 @@
+export default function Avatar() {
+  return (
+    <div className="size-24 rounded-full bg-purple-300 border-4 border-purple-900 "></div>
+  );
+}
---
+++ components/ContactoFooter.jsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+export default function ContactoFooter() {
+  return (
+    <div className="flex p-8 rounded-2xl gap-20 justify-center ">
+      <div className="flex items-center">
+        <img src="/RANDY_08.svg" alt="Randy" className="h-40 w-40 " />
+      </div>
+      <div className="flex-col justify-center">
+        {/* TITLE + SUBTITLE */}
+        <div className="flex-col">
+          <h2 className="font-lucky text-dgreen text-3xl text-center">
+            CONTÁCTANOS
+          </h2>
+          <p className="font-mont text-black font-semibold text-center">
+            ¡Síguenos en nuestras redes sociales!
+          </p>
+        </div>
+
+        {/* ICONOS SOCIALMEDIA */}
+        <div className="flex gap-5 justify-center p-4">
+          <button className="bg-blue-200 p-3 rounded-full">
+            <a href="https://www.facebook.com/">
+              <img src="/facebookblue.svg" alt="G" className="h-8 w-8" />
+            </a>
+          </button>
+          <button className="bg-black p-3 rounded-full">
+            <a href="https://www.tiktok.com/">
+              <img src="/tiktok.svg" alt="G" className="h-8 w-8" />
+            </a>
+          </button>
+          <button className="bg-green-500 p-3 rounded-full">
+            <a href="https://www.spotify.com/">
+              <img src="/spotify.svg" alt="G" className="h-8 w-8" />
+            </a>
+          </button>
+        </div>
+
+        {/* LINKS A AVISOS  */}
+        <div className="flex justify-center gap-10 p-2">
+          <a
+            href=""
+            className="font-mont text-black text-xs hover:font-semibold hover:text-dorange hover:underline"
+          >
+            Privacidad
+          </a>
+          <a
+            href=""
+            className="font-mont text-black text-xs hover:font-semibold hover:text-dorange hover:underline"
+          >
+            Términos y Condiciones
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
