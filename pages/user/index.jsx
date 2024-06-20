173bc53db99db17984546c70b6db8dc1a8241f7e
Sonia Aguirre <arembiam@gmail.com>
2024-06-20 01:50:11 -0600

    user menu structure done needs responsive fix

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -7,26 +7,23 @@ export default function User() {
     <main className="w-full min-h-screen bg-white">
       {/* NAVBAR COMPONENT */}
       <Navbar />
-      {/* BODY */}
-      <div className="m-10 rounded-xl overflow-hidden grid justify-items-center bg-oldwhite h-4/5 w-[350px] md:w-4/5 lg:w-1/2 py-14 md:py-24 px-8 mx-auto">
-        <div className="inline-flex">
-          {/* AVATAR COMPONENT Y NOMBRE USER*/}
-          <div className="">
-            <Avatar />
-          </div>
 
-          {/* SCORE
-          <div className="border-4 h-fit p-4 rounded-xl border-lorange ">
+      <div className="m-10 p-6 grid rounded-xl justify-items-center bg-oldwhite h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
+        <div className="flex-col gap-4 p-4 border border-black">
+          {/* AVATAR COMPONENT Y NOMBRE USER*/}
+          <Avatar />
+          {/* SCORE */}
+          <div className="border-4  p-2 rounded-xl border-lorange ">
             {" "}
-            <p className="font-lucky text-dgreen text-xl text-center">SCORE</p>
-            <p className="font-mont font-semibold text-black text-3xl text-center">
+            <p className="font-lucky text-dgreen text-2xl text-center">SCORE</p>
+            <p className="font-mont font-semibold text-black text-5xl text-center">
               9,999
             </p>
-          </div> */}
+          </div>
         </div>
 
-        {/* BOTONES HACIA SUBMENUS
-        <div className="p-8 flex gap-4 justify-center">
+        {/* BOTONES HACIA SUBMENUS */}
+        <div className="w-fit flex flex-col gap-4 p-2 md:flex-col lg:flex-row">
           <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
             <img src="/icon_userheart.svg" alt="" className="h-8 w-8" />
             <span className="font-lucky text-black text-xl text-center">
@@ -51,7 +48,7 @@ export default function User() {
               NOSOTROS
             </span>
           </div>
-        </div> */}
+        </div>
       </div>
 
       {/* FOOTER CONTACTO */}
