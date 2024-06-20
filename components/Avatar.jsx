89dc42e059e6e06834a15894776628d83db354dc
Sonia Aguirre <arembiam@gmail.com>
2024-06-19 23:23:42 -0600

    fixin avatar component responsive

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,26 +1,23 @@
 export default function Avatar() {
   return (
-    <>
-      <div className="flex flex-col justify-center">
-        {/* RANDY ICON FOR USER DEFAULT ONLY + SETTING ICON */}
-        <div className=" inline-flex gap-2 p-4">
-          <div className="rounded-full bg-lifeL size-20"></div>
+    <div className="">
+      {/* RANDY ICON FOR USER DEFAULT ONLY + SETTING ICON */}
+      <div className="inline-flex p-3">
+        <div className="rounded-full bg-lifeL size-24"></div>
+        <div className="content-end">
           <img src="/icon_redsetting.svg" alt="setting" className="h-6 w-6 " />
         </div>
+      </div>
 
-        {/* CARD USER NAME + SETTING ICON */}
-        <div className=" inline-flex gap-2 p-4">
-          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 align-middle text-center">
-            <p className="font-lucky text-black">JuanElChato92</p>
-          </div>
-
-          <img
-            src="/icon_purplesetting.svg"
-            alt="setting"
-            className="h-6 w-6 "
-          />
+      {/* CARD USER NAME + SETTING ICON */}
+      <div className="inline-flex">
+        <div className="bg-lorange rounded-lg py-2 px-4 text-center">
+          <p className="font-lucky text-black">JuanElChato92</p>
+        </div>
+        <div className="content-end">
+          <img src="/icon_purplesetting.svg" alt="setting" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -8,60 +8,54 @@ export default function User() {
       {/* NAVBAR COMPONENT */}
       <Navbar />
       {/* BODY */}
-      <div className="p-4">
-        <div className="grid justify-items-center bg-oldwhite h-4/5 w-[350px] md:w-4/5 lg:w-1/2 py-14 md:py-24 px-8 mx-auto ">
-          <div className="inline-flex gap-10 justify-center p-10">
-            {/* AVATAR COMPONENT Y NOMBRE USER*/}
-            <div className="">
-              <Avatar />
-            </div>
-
-            {/* SCORE */}
-            <div className="border-4 py-2 px-6 rounded-xl border-lorange content-center ">
-              {" "}
-              <p className="font-lucky text-dgreen text-3xl text-center">
-                SCORE
-              </p>
-              <p className="font-mont font-semibold text-black text-6xl text-center">
-                9,999
-              </p>
-            </div>
+      <div className="m-10 rounded-xl overflow-hidden grid justify-items-center bg-oldwhite h-4/5 w-[350px] md:w-4/5 lg:w-1/2 py-14 md:py-24 px-8 mx-auto">
+        <div className="inline-flex">
+          {/* AVATAR COMPONENT Y NOMBRE USER*/}
+          <div className="">
+            <Avatar />
           </div>
 
-          {/* BOTONES HACIA SUBMENUS */}
-          <div className="p-8 flex gap-4 justify-center">
-            <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
-              <img src="/icon_userheart.svg" alt="" className="h-8 w-8" />
-              <span className="font-lucky text-black text-xl text-center">
-                FAVS
-              </span>
-            </div>
-            <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
-              <img src="/icon_userachieve.svg" alt="" className="h-8 w-8" />
-              <span className="font-lucky text-black text-xl text-center">
-                LOGROS
-              </span>
-            </div>
-            <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
-              <img src="/icon_userview.svg" alt="" className="h-8 w-8" />
-              <span className="font-lucky text-black text-xl text-center">
-                VISTOS
-              </span>
-            </div>
-            <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
-              <img src="/icon_userabt.svg" alt="" className="h-8 w-8" />
-              <span className="font-lucky text-black text-xl text-center">
-                NOSOTROS
-              </span>
-            </div>
+          {/* SCORE */}
+          <div className="border-4 h-fit p-4 rounded-xl border-lorange ">
+            {" "}
+            <p className="font-lucky text-dgreen text-xl text-center">SCORE</p>
+            <p className="font-mont font-semibold text-black text-3xl text-center">
+              9,999
+            </p>
           </div>
         </div>
+
+        {/* BOTONES HACIA SUBMENUS
+        <div className="p-8 flex gap-4 justify-center">
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
+            <img src="/icon_userheart.svg" alt="" className="h-8 w-8" />
+            <span className="font-lucky text-black text-xl text-center">
+              FAVS
+            </span>
+          </div>
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
+            <img src="/icon_userachieve.svg" alt="" className="h-8 w-8" />
+            <span className="font-lucky text-black text-xl text-center">
+              LOGROS
+            </span>
+          </div>
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
+            <img src="/icon_userview.svg" alt="" className="h-8 w-8" />
+            <span className="font-lucky text-black text-xl text-center">
+              VISTOS
+            </span>
+          </div>
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-10 ">
+            <img src="/icon_userabt.svg" alt="" className="h-8 w-8" />
+            <span className="font-lucky text-black text-xl text-center">
+              NOSOTROS
+            </span>
+          </div>
+        </div> */}
       </div>
 
       {/* FOOTER CONTACTO */}
-      <div className="">
-        <ContactoFooter />
-      </div>
+      <ContactoFooter />
     </main>
   );
 }
