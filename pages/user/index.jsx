debc3731171fb4ceb1d34b645d2c30a4143c3702
Sonia Aguirre <arembiam@gmail.com>
2024-06-20 19:10:10 -0600

    avatar comp fix, user menu responsive fix

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -4,19 +4,19 @@ import ContactoFooter from "@/components/ContactoFooter";
 
 export default function User() {
   return (
-    <main className="w-full min-h-screen bg-white">
+    <main className="w-full min-h-screen bg-white overflow-hidden">
       {/* NAVBAR COMPONENT */}
       <Navbar />
       {/* CARD CONTAINER */}
       <div className=" bg-oldwhite rounded-xl p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
         <div className="md:grid grid-cols-2 ">
           {/* AVATAR COMPONENT Y NOMBRE USER*/}
-          <div className="border border-black p-4 px-10">
+          <div className="p-4 px-10 lg:px-2">
             <Avatar />
           </div>
 
           {/* SCORE */}
-          <div className="border border-black py-4 px-10 grid grid-cols-1 place-items-center">
+          <div className="py-4 px-10 grid grid-cols-1 place-items-center lg:px-2">
             <div className="border-4 border-lorange rounded-lg py-2 px-4 grid grid-rows-2 gap-2 w-fit">
               {" "}
               <p className="font-lucky text-dgreen text-2xl  text-center">
@@ -30,24 +30,34 @@ export default function User() {
         </div>
 
         {/* BOTONES HACIA SUBMENUS */}
-        <div className="border border-black grid gap-4 w-full py-4 px-10 md:px-28">
-          <div className="bg-lorange rounded-lg py-2 px-4 grid grid-cols-2 gap-2 place-items-center">
-            <img src="/icon_userheart.svg" alt="" className="h-8 w-8" />
-
-            <span className="font-lucky text-black text-xl ">FAVS</span>
+        <div className="grid gap-4 w-full py-4 px-10 md:px-32 lg:grid-cols-2 lg:gap-2 lg:px-2 xl:grid-cols-4 ">
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
+            <img src="/icon_userheart.svg" alt="" className="h-8 w-8 " />
+            <span className="font-lucky text-black text-xl xl:text-lg">
+              FAVS
+            </span>
           </div>
 
-          <div className="bg-lorange rounded-lg py-2 px-4 grid grid-cols-2  gap-2 place-items-center">
-            <img src="/icon_userachieve.svg" alt="" className="h-8 w-8" />
-            <span className="font-lucky text-black text-xl ">LOGROS</span>
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
+            <img src="/icon_userachieve.svg" alt="" className="h-8 w-8 " />
+            <span className="font-lucky text-black text-xl xl:text-lg">
+              LOGROS
+            </span>
           </div>
-          <div className="bg-lorange rounded-lg py-2 px-4 grid grid-cols-2  gap-2 place-items-center">
-            <img src="/icon_userview.svg" alt="" className="h-8 w-8" />
-            <span className="font-lucky text-black text-xl ">VISTOS</span>
+
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
+            <img src="/icon_userview.svg" alt="" className="h-8 w-8 " />
+            <span className="font-lucky text-black text-xl xl:text-lg ">
+              VISTOS
+            </span>
           </div>
-          <div className="bg-lorange rounded-lg py-2 px-4 grid grid-cols-2  gap-2 place-items-center">
+
+          <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
             <img src="/icon_userabt.svg" alt="" className="h-8 w-8" />
-            <span className="font-lucky text-black text-xl ">NOSOTROS</span>
+
+            <span className="font-lucky text-black text-xl xl:text-lg">
+              NOSOTROS
+            </span>
           </div>
         </div>
       </div>
