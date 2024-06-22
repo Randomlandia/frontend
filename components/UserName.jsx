3a894456b438aaa1a8c02e31b4d6bb8a9c494a2c
Sonia Aguirre <arembiam@gmail.com>
2024-06-21 21:14:51 -0600

    new clone

--- components/UserName.jsx
+++ components/UserName.jsx
@@ -1,11 +1,19 @@
 export default function UserName() {
   return (
     <div className=" bg-oldwhite rounded-xl p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
-      <p className="font-lucky text-dgreen text-center text-3xl">
-        ¡Escribe tu nombre!
-      </p>
-      <div>
-        <p>Gatito-3000</p>
+      <div className="p-4">
+        <p className="font-lucky text-dgreen text-center text-3xl">
+          ¡Escribe tu nombre!
+        </p>
+      </div>
+
+      <div className=" rounded-lg border-4 border-lorange bg-white bg-opacity-45 p-3 ">
+        <div>
+          <p className="font-mont text-black font-semibold text-center text-6xl">
+            Gatito-3000
+          </p>
+        </div>
+        <div></div>
       </div>
     </div>
   );
