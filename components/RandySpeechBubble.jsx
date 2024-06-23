d782e9fbbeffd8fab2c704ca30c528c389fe1f6a
aluzdev <aluzdev@gmail.com>
2024-06-22 23:17:18 -0600

    added speechBubble without randy

--- components/RandySpeechBubble.jsx
+++ components/RandySpeechBubble.jsx
@@ -3,11 +3,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-const SpeechBubble = ({ text, trianglePosition }) => {
+const RandySpeechBubble = ({ text, trianglePosition, imageSrc, imageAlt }) => {
   const triangleClasses = classNames({
     "absolute bottom-[-10px] w-0 h-0 border-t-[10px] border-t-cream border-x-[10px] border-x-transparent": true,
     "left-6": trianglePosition === "left",
     "right-6": trianglePosition === "right",
+  });
+
+  const randyClasses = classNames({
+    "absolute bottom-[-106px] w-24 h-auto": true,
+    "left-[-0.75rem]": trianglePosition === "left",
+    "right-[-0.75rem]": trianglePosition === "right",
   });
 
   return (
@@ -19,17 +25,22 @@ const SpeechBubble = ({ text, trianglePosition }) => {
     >
       <div className="overflow-y-auto h-full speech-bubble">{text}</div>
       <div className={triangleClasses}></div>
+      <img src={imageSrc} alt={imageAlt} className={randyClasses} />
     </div>
   );
 };
 
-SpeechBubble.propTypes = {
+RandySpeechBubble.propTypes = {
   text: PropTypes.string.isRequired,
   trianglePosition: PropTypes.oneOf(["left", "right"]),
+  imageSrc: PropTypes.string,
+  imageAlt: PropTypes.string,
 };
 
-SpeechBubble.defaultProps = {
+RandySpeechBubble.defaultProps = {
   trianglePosition: "left",
+  imageSrc: "/randy.png", // Default image path
+  imageAlt: "Randy the cat looking happy and content", // Default alt text
 };
 
-export default SpeechBubble;
+export default RandySpeechBubble;
--- pages/menu/[topic].jsx
+++ pages/menu/[topic].jsx
@@ -1,9 +1,13 @@
+import RandySpeechBubble from "@/components/RandySpeechBubble";
 import SpeechBubble from "@/components/SpeechBubble";
 
 export default function Sandia() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <SpeechBubble trianglePosition="right" text="hi"></SpeechBubble>
+      <SpeechBubble
+        trianglePosition="right"
+        text="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+      ></SpeechBubble>
     </div>
   );
 }
