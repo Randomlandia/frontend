dd275fb0d01fd8605fba1071442d9285a6caa4ac
Sonia Aguirre <arembiam@gmail.com>
2024-06-25 21:57:55 -0600

    modal user name ready, no hook forms only design

--- components/UserName.jsx
+++ components/UserName.jsx
@@ -1,8 +1,14 @@
 export default function UserName() {
   return (
-    <div className="flex flex-col bg-dorange bg-opacity-45 rounded-xl p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
+    <div className="flex flex-col gap-2 bg-oldwhite rounded-xl p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
+      {/* BOTÓN CERRAR */}
       <div className="flex flex-row justify-end">
-        <img src="/close.svg" alt="close" className="h-4 w-4" />
+        <button
+          type=""
+          className="rounded-full w-fit border-2 border-transparent hover:border-red-500 p-2"
+        >
+          <img src="/close.svg" alt="close" className="h-4 w-4" />
+        </button>
       </div>
 
       {/* TITULO DEL MODAL */}
@@ -13,14 +19,20 @@ export default function UserName() {
       </div>
 
       {/* INPUT PARA NOMBRE USER */}
-      <div className=" flex flex-col rounded-xl border-4 border-lorange bg-white bg-opacity-45 p-2">
-        <p className="font-mont text-black font-semibold text-center text-4xl">
-          Gatito-3000
-        </p>
+      <div className=" flex flex-col rounded-xl border-4 border-lorange p-2">
+        <input
+          type="text"
+          className="placeholder:text-black font-mont bg-transparent text-center text-xl focus:outline-none focus:ring-2 focus:ring-lorange"
+          placeholder="Randy es genial"
+        />
       </div>
 
       {/* BOTÓN GUARDAR */}
-      <div></div>
+      <div className="flex justify-center p-2">
+        <button className="p-2 w-fit rounded-lg border-2 bg-pcyan hover:border-blue-600  ">
+          <p className="font-lucky text-white text-center">GUARDAR</p>
+        </button>
+      </div>
     </div>
   );
 }
