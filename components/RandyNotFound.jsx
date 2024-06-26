977d9839575531ed6dcb2fedc2ac36af0de4dd36
Sonia Aguirre <arembiam@gmail.com>
2024-06-26 11:12:43 -0600

    ERROR404 component done

--- components/RandyNotFound.jsx
+++ components/RandyNotFound.jsx
@@ -1,21 +1,30 @@
 export default function RandyNotFound() {
   return (
-    <div className="flex flex-col p-10">
+    <div className="flex flex-col p-10 overflow-hidden">
       {/* TITULO DEL ERROR 404 */}
       <div className="flex-wrap justify-center p-3">
-        <p className="font-mono text-black text-center font-extrabold text-6xl">
-          ERROR
+        <p className="font-mono text-zinc-500 text-center font-extrabold text-8xl">
+          4 0 4
         </p>
-        <p className="font-mono text-black text-center font-extrabold text-8xl">
-          404
-        </p>
-        <p className="font-mono text-black text-center font-extrabold text-2xl">
-          RANDY NOT FOUND
+        <p className="font-mono text-zinc-500 text-center  text-lg">
+          Randy no pudo encontrar la página que estabas buscando
         </p>
       </div>
       {/* RANDY */}
       <div className="flex justify-center p-4">
-        <img src="/randy_wink.svg" alt="" className="h-22 w-22" />
+        <img
+          src="/RANDY_NOTFOUND.svg"
+          alt="RandyNotFound"
+          className="h-52 w-auto"
+        />
+      </div>
+      <div className="flex justify-center ">
+        <a
+          href="/"
+          className="font-mont font-semibold text-zinc-500 hover:text-pcyan hover:underline text-center text-lg "
+        >
+          Regresar al inicio
+        </a>
       </div>
     </div>
   );
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,9 @@
-import RandyNotFound from "@/components/RandyNotFound";
 import Navbar from "../components/Navbar";
 
 export default function About() {
   return (
     <main className="w-full min-h-screen bg-white">
       <Navbar />
-      <div>
-        <RandyNotFound />
-      </div>
     </main>
   );
 }
