b6b71be7aae04d26e7357f1f7ea1bcd527df0a25
Sonia Aguirre <arembiam@gmail.com>
2024-06-26 22:42:43 -0600

    Medallas index design done, responsive missing need carrousel

--- pages/user/achv/index.jsx
+++ pages/user/achv/index.jsx
@@ -1,6 +1,4 @@
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function achv() {
@@ -24,8 +22,8 @@ export default function achv() {
 
       <div className="flex justify-center p-5">
         {/* CONTAINER DE MEDALLAS X TOPIC*/}
-        <div className=" grid grid-cols-4 gap- justify-items-center gap-2 bg-oldwhite/70 shadow-md rounded-xl p-5 w-fit">
-          {/* CONTAINER */}
+        <div className=" grid sm:grid-cols-1 md:grid-cols-2 md:p-10 md:gap-6 lg:grid-cols-4 lg:gap-3 lg:p-5 justify-items-center bg-oldwhite/70 shadow-md rounded-xl p-5 ">
+          {/* CONTAINER IDIOMAS */}
           <div className="flex-col justify-center w-fit">
             {/* TITLE DEL TOPIC */}
             <div className="flex-col p-2">
@@ -36,129 +34,121 @@ export default function achv() {
 
             {/* CARD MEDALLAS */}
             <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl shadow-md">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
+              <img src="/B_IDIOMASgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_IDIOMASgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_IDIOMASgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_IDIOMASgrey.svg" alt="" className="h-10 w-10" />
             </div>
           </div>
-          {/* CONTAINER */}
-          <div className="flex-col justify-center w-fit">
+          {/* CONTAINER MATE */}
+          <div className="flex-col justify-center">
             {/* TITLE DEL TOPIC */}
             <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
+              <p className="font-lucky text-mathD text-2xl text-center">MATE</p>
+            </div>
+
+            {/* CARD MEDALLAS */}
+            <div className=" grid grid-cols-2 gap-2 border-4 border-mathL bg-white/35 p-2  rounded-xl shadow-md">
+              <img src="/B_MATEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_MATEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_MATEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_MATEgrey.svg" alt="" className="h-10 w-10" />
+            </div>
+          </div>
+          {/* CONTAINER CIENCIAS*/}
+          <div className="flex-col justify-center ">
+            {/* TITLE DEL TOPIC */}
+            <div className="flex-col p-2">
+              <p className="font-lucky text-natD text-2xl text-center">
+                CIENCIAS
               </p>
             </div>
 
             {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
+            <div className=" grid grid-cols-2 gap-2 border-4 border-natL bg-white/35 p-2  rounded-xl shadow-md">
+              <img src="/B_CIENCIAgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_CIENCIAgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_CIENCIAgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_CIENCIAgrey.svg" alt="" className="h-10 w-10" />
             </div>
           </div>
-          {/* CONTAINER */}
+          {/* CONTAINER MUNDO */}
           <div className="flex-col justify-center w-fit">
             {/* TITLE DEL TOPIC */}
             <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
+              <p className="font-lucky text-worldD text-2xl text-center">
+                MUNDO
               </p>
             </div>
 
             {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
+            <div className=" grid grid-cols-2 gap-2 border-4 border-worldL bg-white/35 p-2  rounded-xl shadow-md">
+              <img src="/B_MUNDOgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_MUNDOgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_MUNDOgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_MUNDOgrey.svg" alt="" className="h-10 w-10" />
             </div>
           </div>
-          {/* CONTAINER */}
-          <div className="flex-col justify-center w-fit">
+          {/* CONTAINER NERD */}
+          <div className="flex-col justify-center ">
             {/* TITLE DEL TOPIC */}
             <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
+              <p className="font-lucky text-nerdD text-2xl text-center">NERD</p>
+            </div>
+
+            {/* CARD MEDALLAS */}
+            <div className=" grid grid-cols-2 gap-2 border-4 border-nerdL bg-white/35 p-2  rounded-xl shadow-md">
+              <img src="/B_NERDgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_NERDgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_NERDgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_NERDgrey.svg" alt="" className="h-10 w-10" />
+            </div>
+          </div>
+          {/* CONTAINER DEPORTES*/}
+          <div className="flex-col justify-center ">
+            {/* TITLE DEL TOPIC */}
+            <div className="flex-col p-2">
+              <p className="font-lucky text-sportD text-2xl text-center">
+                DEPORTES
               </p>
             </div>
 
             {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
+            <div className=" grid grid-cols-2 gap-2 border-4 border-sportL bg-white/35 p-2  rounded-xl">
+              <img src="/B_DEPORTEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_DEPORTEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_DEPORTEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_DEPORTEgrey.svg" alt="" className="h-10 w-10" />
             </div>
           </div>
-          {/* CONTAINER */}
-          <div className="flex-col justify-center w-fit">
+          {/* CONTAINER VIDA */}
+          <div className="flex-col justify-center ">
             {/* TITLE DEL TOPIC */}
             <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
-              </p>
+              <p className="font-lucky text-lifeD text-2xl text-center">VIDA</p>
             </div>
 
             {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
+            <div className=" grid grid-cols-2 gap-2 border-4 border-lifeL bg-white/35 p-2  rounded-xl shadow-md">
+              <img src="/B_VIDAgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_VIDAgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_VIDAgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_VIDAgrey.svg" alt="" className="h-10 w-10" />
             </div>
           </div>
-          {/* CONTAINER */}
-          <div className="flex-col justify-center w-fit">
+          {/* CONTAINER ARTE*/}
+          <div className="flex-col justify-center">
             {/* TITLE DEL TOPIC */}
             <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
-              </p>
+              <p className="font-lucky text-artD text-2xl text-center">ARTE</p>
             </div>
 
             {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-            </div>
-          </div>
-          {/* CONTAINER */}
-          <div className="flex-col justify-center w-fit">
-            {/* TITLE DEL TOPIC */}
-            <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
-              </p>
-            </div>
-
-            {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-            </div>
-          </div>
-          {/* CONTAINER */}
-          <div className="flex-col justify-center w-fit">
-            {/* TITLE DEL TOPIC */}
-            <div className="flex-col p-2">
-              <p className="font-lucky text-langD text-2xl text-center">
-                IDIOMAS
-              </p>
-            </div>
-
-            {/* CARD MEDALLAS */}
-            <div className=" grid grid-cols-2 gap-2 border-4 border-langL bg-white/35 p-2  rounded-xl">
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
-              <img src="/B_IDIOMASgrey.svg" alt="" />
+            <div className=" grid grid-cols-2 gap-2 border-4 border-artL bg-white/35 p-2  rounded-xl shadow-md">
+              <img src="/B_ARTEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_ARTEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_ARTEgrey.svg" alt="" className="h-10 w-10" />
+              <img src="/B_ARTEgrey.svg" alt="" className="h-10 w-10" />
             </div>
           </div>
         </div>
