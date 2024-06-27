eb42f1e1c8daccdea3821dd07a77d21bfabde13d
Sonia Aguirre <arembiam@gmail.com>
2024-06-27 17:16:01 -0600

    Medalla Detail structure

--- components/modalsMedals/MedallaDetail.jsx
+++ components/modalsMedals/MedallaDetail.jsx
@@ -1,3 +1,12 @@
 export default function MedallaDetail() {
-  return <div>x</div>;
+  return (
+    <div className="flex flex-col gap-2 bg-oldwhite/75 border-4 border-natL rounded-xl shadow-lg p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
+      {/* NOMBRE DEL TOPIC */}
+      <div className="flex justify-center">
+        <p className="font-lucky text-natD text-center text-4xl">CIENCIAS</p>
+      </div>
+
+      {/* MEDALLAS */}
+    </div>
+  );
 }
