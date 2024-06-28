660ed4c1f1c5a40e2f9daa2be4b14ae9bbf90702
aluzdev <aluzdev@gmail.com>
2024-06-28 03:02:19 -0600

    Speech bubble (#22)
    * speech bubble component
    
    * added randy to speech bubble
    
    * any image can be added
    
    * removed unused code
    
    * added speechBubble without randy
    
    * both speech bubbles
    
    * sandia section without background
    
    * refactor

---
+++ components/SpeechBubble.jsx
@@ -0,0 +1,44 @@
+// components/SpeechBubble.js
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+const SpeechBubble = ({ text, trianglePosition, width, height }) => {
+  const triangleClasses = classNames({
+    "absolute bottom-[-10px] w-0 h-0 border-t-[10px] border-t-cream border-x-[10px] border-x-transparent": true,
+    "left-6": trianglePosition === "left",
+    "right-6": trianglePosition === "right",
+  });
+
+  const bubbleStyles = {
+    width: width,
+    height: height,
+  };
+
+  return (
+    <div
+      className="relative bg-cream text-black p-4 rounded-lg"
+      style={bubbleStyles}
+    >
+      <div className="overflow-y-auto h-full speech-bubble text-center">
+        {text}
+      </div>
+      <div className={triangleClasses}></div>
+    </div>
+  );
+};
+
+SpeechBubble.propTypes = {
+  text: PropTypes.string.isRequired,
+  trianglePosition: PropTypes.oneOf(["left", "right"]),
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+SpeechBubble.defaultProps = {
+  trianglePosition: "left",
+  width: "332.25px", // Default width
+  height: "329.34px", // Default height
+};
+
+export default SpeechBubble;
--- pages/menu/[topic].jsx
+++ pages/menu/[topic].jsx
@@ -1,7 +1,45 @@
-export default function Sandia () {
+import Navbar from "@/components/Navbar";
+import RandySpeechBubble from "@/components/RandySpeechBubble";
+import SandiaIcon from "@/components/SandiaIcon";
+
+export default function Sandia() {
+  const icons = [
+    { src: "/icon_turn.svg", alt: "Turn Icon" },
+    { src: "/icon_redheart.svg", alt: "Red Heart Icon" },
+    { src: "/icon_arrowleft.svg", alt: "Arrow Left Icon" },
+    { src: "/icon_turnright.svg", alt: "Turn Right Icon" },
+  ];
+
   return (
-    <p>
-      Randy muestra las sandias
-    </p>
-  )
+    <>
+      <Navbar />
+      <section
+        id="card-sandia"
+        className="bg-cream/40 min-h-[656px] min-w-[360px] p-3"
+      >
+        <div className="max-w-sm mx-auto flex flex-col justify-center items-center gap-8">
+          <div className="flex justify-between w-[88%]">
+            <SandiaIcon
+              src="/yellow.png"
+              alt="Yellow Icon"
+              width={77}
+              height={77}
+            />
+            <SandiaIcon
+              src="/close.svg"
+              alt="Close Icon"
+              width={40}
+              height={40}
+            />
+          </div>
+          <RandySpeechBubble text="Sabías que... una piña tarda de 1.5 a 3 años en crecer?" />
+          <div className="grid grid-cols-2 gap-2 h-32 w-36 ml-[68%]">
+            {icons.map((icon, index) => (
+              <SandiaIcon key={index} src={icon.src} alt={icon.alt} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
