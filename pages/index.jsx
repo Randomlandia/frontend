41979aa2dd2aae0082b923e4fe0780288908f323
Sonia Aguirre <arembiam@gmail.com>
2024-07-04 19:11:16 -0600

    Card Sandia back update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,11 @@
 import Navbar from "../components/Navbar";
 import CardSandiaBack from "@/backups/cards sandia/CardSandiaBack";
-
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-white">
       <Navbar />
       <div className=" flex text-orange-600 font-semibold justify-center mt-8 text-5xl">
         Home
-      </div>
-      <div className="flex justify-center p-10">
-        <CardSandiaBack />
       </div>
     </main>
   );
