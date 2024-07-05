92a72687562275cea22fa08f89ac7298d37210ff
Jesica Reyes <miucomosmos333@gmail.com>
2024-07-04 19:28:58 -0600

    fix: letra blanca a negra

--- utils/scrollableBox.jsx
+++ utils/scrollableBox.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/ScrollableBox.module.css";
 
 const useScrollCheck = (content) => {
@@ -9,15 +9,17 @@ const useScrollCheck = (content) => {
   useEffect(() => {
     const checkScroll = () => {
       if (containerRef.current && contentRef.current) {
-        setNeedsScroll(contentRef.current.scrollHeight > containerRef.current.clientHeight);
+        setNeedsScroll(
+          contentRef.current.scrollHeight > containerRef.current.clientHeight
+        );
       }
     };
 
     checkScroll();
-    window.addEventListener('resize', checkScroll);
+    window.addEventListener("resize", checkScroll);
 
     return () => {
-      window.removeEventListener('resize', checkScroll);
+      window.removeEventListener("resize", checkScroll);
     };
   }, [content]);
 
@@ -28,15 +30,23 @@ export default function ScrollableBox({ content }) {
   const { containerRef, contentRef, needsScroll } = useScrollCheck(content);
 
   return (
-    <div className="relative h-64 w-64 overflow-hidden">
-      <div ref={containerRef} className="relative h-full w-full overflow-hidden">
-        <div ref={contentRef} className={`absolute inset-0 overflow-y-auto ${styles.scrollbarHide}`}>
+    <div className="relative h-64 w-64 overflow-hidden ">
+      <div
+        ref={containerRef}
+        className="relative h-full w-full overflow-hidden"
+      >
+        <div
+          ref={contentRef}
+          className={`absolute inset-0 overflow-y-auto ${styles.scrollbarHide}`}
+        >
           <div className={`space-y-1 ${styles.content}`}>
-            <p>{content}</p>
+            <p className="text-black">{content}</p>
           </div>
         </div>
         {needsScroll && (
-          <div className={`arrow-container animate-bounce ${styles.arrowContainer}`}>
+          <div
+            className={`arrow-container animate-bounce ${styles.arrowContainer}`}
+          >
             <div className={`arrow ${styles.arrow}`}></div>
           </div>
         )}
