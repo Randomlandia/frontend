5e655356d61d301d037a4b450e2eea8d9ada4216
Jesica Reyes <miucomosmos333@gmail.com>
2024-07-05 05:51:55 -0600

    feat: prototipo chispitas

---
+++ components/home/luz.jsx
@@ -0,0 +1,25 @@
+export default function Luz() {
+  return (
+    <div className=" flex min-h-[100vh] m-0 p-0">
+      <div
+        id="contenedorLuz"
+        className="relative w-[100%] h-[100vh] overflow-hidden "
+      >
+        <div class="burbujas" className=" flex relative">
+          <div class="burbuja" className=" "></div>
+          <div class="burbujaDos" className=""></div>
+          <div class="burbujaTres" className=""></div>
+          <div class="burbujaCuatro" className=""></div>
+          <div class="burbujaCinco" className=""></div>
+          <div class="burbujaSeis" className=""></div>
+          <div class="burbujaSiete" className=" "></div>
+          <div class="burbujaOcho" className=" "></div>
+          <div class="burbujaNueve" className=" "></div>
+          <div class="burbujaDiez" className=" "></div>
+          <div class="burbujaOnce" className=" "></div>
+          <div class="burbujaDoce" className=" "></div>
+        </div>
+      </div>
+    </div>
+  );
+}
