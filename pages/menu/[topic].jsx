7c0468889c5577aa49d43403f12ecbff48d3f01a
angelagar <ggarcia.angelad@gmail.com>
2024-07-10 15:52:01 -0600

    fix: randomSandia page, useCases- add to view remove from viewList

--- pages/menu/[topic].jsx
+++ pages/menu/[topic].jsx
@@ -9,16 +9,26 @@ import Link from "next/link";
 // 2 un contador que llegue a 10
 
 export default function Sandia() {
+  const router = useRouter();
+  const [sandiasByTopic, setSandiasByTopic] = useState([]);
   const [seenSandias, setSeenSandias] = useState([]);
   const [favs, setFavs] = useState([]);
   const [reverseSandias, setReverseSandias] = useState([]);
   const [flecha, setFlecha] = useState(false);
   const [contador, setContador] = useState(1);
   const [texto, setTexto] = useState("no hay mas por mostrar!!!");
+  const [loading, setLoading] = useState(true);
+  const [testCt, setTestCt] = useState(0);
+  const [showTest, setShowTest] = useState(false);
+  const [current, setCurrent] = useState(null);
+  let { topic } = router.query;
 
-  
   const addSandia = (newSandia) => {
-    setSeenSandias((prevSeenSandias) => [...prevSeenSandias, newSandia]);
+    setSeenSandias((prevSeenSandias) => {
+      const updatedSeenSandias = [...prevSeenSandias, newSandia];
+      localStorage.setItem("view", JSON.stringify(updatedSeenSandias));
+      return updatedSeenSandias;
+    });
   };
 
   const addReverseSandias = (newSandia) => {
@@ -26,13 +36,20 @@ export default function Sandia() {
   };
 
   const getRandomSandia = () => {
-    const sandias = sandiasByTopic.data.sandias;
-    const randomSandiaIndex = Math.floor(Math.random() * sandias.length);
-    const randomSandia = sandias[randomSandiaIndex];
-    addSandia(randomSandia);
-    addReverseSandias(randomSandia);
-    setFlecha(true);
-    setContador(1);
+    if (sandiasByTopic.length === 0) return
+    const randomSandiaIndex = Math.floor(Math.random() * sandiasByTopic.length);
+    const randomSandia = sandiasByTopic[randomSandiaIndex];
+
+    if (testCt === 10 && topic !== "default") {
+      setShowTest(true);
+    }
+
+    if (showTest) {
+      //funcion que ejecuta el test
+      setTestCt(0);
+      setShowTest(false);
+    }
+    return randomSandia;
   };
 
   const reverseSandia = () => {
@@ -47,56 +64,65 @@ export default function Sandia() {
     }
   };
 
-  const icons = [
-    { src: "/icon_turn.svg", alt: "Turn Icon" },
-    { src: "/icon_redheart.svg", alt: "Red Heart Icon" },
-    {
-      src: "/icon_arrowleft.svg",
-      alt: "Arrow Left Icon",
-      onClick: reverseSandia,
-    },
-    {
-      src: "/icon_turnright.svg",
-      alt: "Turn Right Icon",
-      onClick: getRandomSandia,
-    },
-  ];
+  const updatedSandiasByTopic = (viewedSandia) => {
+    const newList = sandiasByTopic.filter(
+      (sandia) => sandia._id !== viewedSandia._id
+    );
+    setSandiasByTopic(newList);
+    localStorage.setItem("Sandias", JSON.stringify(newList));
+  };
 
-  const router = useRouter();
-  const [sandiasByTopic, setSandiasByTopic] = useState({
-    data: { sandias: [] },
-  });
-  const [loading, setLoading] = useState(true);
-  let topic = router.query.topic;
+  const handleNextButton = () => {
+    const randomSandia = getRandomSandia();
+    if (!randomSandia) {
+      console.log("No hay más sandías disponibles.");
+      setCurrent({ content: "No hay más sandías disponibles. Explora otros temas o revisa lo que ya has visto." });
+      return null;
+    };
+
+    addSandia(randomSandia);
+    addReverseSandias(randomSandia);
+    setFlecha(true);
+    setContador(1);
+    setTestCt(testCt + 1);
+    console.log(randomSandia);
+    setCurrent(randomSandia);
+    updatedSandiasByTopic(randomSandia);
+  };
 
   useEffect(() => {
     const storedFavs = JSON.parse(localStorage.getItem("favs")) || [];
     setFavs(storedFavs);
+    const storedSeenSandias = JSON.parse(localStorage.getItem("view")) || [];
+    setSeenSandias(storedSeenSandias);
 
     if (topic) {
-      fetch(`http://localhost:3005/sandias/topic/${topic}`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((json) => {
-          setSandiasByTopic(json);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-          setLoading(false);
-        });
+      const sandias = JSON.parse(localStorage.getItem("Sandias")) || [];
+
+      let filteredSandias;
+      if (topic === "default") {
+        filteredSandias = sandias;
+      } else {
+        filteredSandias = sandias.filter(
+          (sandia) => sandia?.topic?.name === topic
+        );
+      }
+
+      const filterSandiasExcSeen = filteredSandias?.filter(
+        (sandia) =>
+          !storedSeenSandias.some((seenSandia) => seenSandia?._id == sandia._id)
+      );
+
+      setSandiasByTopic(filterSandiasExcSeen);
+      setLoading(false);
     }
   }, [topic]);
 
   useEffect(() => {
-    if (!loading && sandiasByTopic.data.sandias.length > 0) {
+    if (sandiasByTopic.length > 0) {
       getRandomSandia();
     }
-  }, [loading, sandiasByTopic]);
+  }, [sandiasByTopic]);
 
   if (loading) {
     return (
@@ -104,16 +130,15 @@ export default function Sandia() {
     );
   }
 
-  if (!sandiasByTopic.data || sandiasByTopic.data.sandias.length === 0) {
-    return (
-      <main className="w-full h-full bg-white min-h-screen min-w-full">
-        <Navbar />
-        <section className="flex items-center justify-center min-h-screen">
-          <p>No sandias found for this topic.</p>
-        </section>
-      </main>
-    );
-  }
+  // if (!sandiasByTopic.data || sandiasByTopic.data.sandias.length === 0) {
+  //   return (
+  //     <main className="w-full h-full bg-white min-h-screen min-w-full">
+  //       <Navbar />
+  //       <section className="flex items-center justify-center min-h-screen">
+  //         <p>No sandias found for this topic.</p>
+  //       </section>
+  //     </main>
+  //   );
 
   return (
     <>
@@ -140,28 +165,27 @@ export default function Sandia() {
             </Link>
           </div>
 
-          {seenSandias[seenSandias.length - 1]?.content && (
-            <RandySpeechBubble
-              text={
-                flecha === true && seenSandias[seenSandias.length - 1].content
-              }
-              text1={
-                (flecha === false && texto.content) || "no hay más que mostrar"
-              }
-            />
-          )}
+          <RandySpeechBubble text={current?.content} />
 
           <div className="grid grid-cols-2 gap-2 h-32 w-36 ml-[68%]">
-            {icons.map((icon, index) => (
-              <SandiaIcon
-                key={index}
-                src={icon.src}
-                alt={icon.alt}
-                onClick={() =>
-                  icon.onClick(seenSandias[seenSandias.length - 1])
-                }
-              />
-            ))}
+            <button key="turnIcon">
+              <img src="/icon_turn.svg" alt="Turn Icon" />
+            </button>
+
+            <button key="redHeartIcon">
+              <img src="/icon_redheart.svg" alt="Red Heart Icon" />
+            </button>
+
+            <button
+              key="arrowLeftIcon"
+              onClick={() => reverseSandia(seenSandias[seenSandias.length - 1])}
+            >
+              <img src="/icon_arrowleft.svg" alt="Arrow Left Icon" />
+            </button>
+
+            <button key="turnRightIcon" onClick={handleNextButton}>
+              <img src="/icon_turnright.svg" alt="Turn Right Icon" />
+            </button>
           </div>
         </div>
       </section>
