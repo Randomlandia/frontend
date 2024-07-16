112f4396d92cfe62888a09fffa9e91b483c619d8
Sonia Aguirre <arembiam@gmail.com>
2024-07-16 13:19:35 -0600

    aviso de privacidad new page

---
+++ pages/avisoprivacidad.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+export default function AvisoPrivacidad() {
+  return (
+    <main className="w-full min-h-screen bg-white">
+      <Navbar />
+      <div className=" flex text-orange-600 font-semibold justify-center mt-8 text-5xl">
+        PRUEBAS
+      </div>
+    </main>
+  );
+}
