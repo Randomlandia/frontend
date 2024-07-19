4c7a6eef944984072c80fbc4ba458d1ff2351c27
Sonia Aguirre <arembiam@gmail.com>
2024-07-19 00:16:03 -0600

    Modal structure update, needs format and logic

--- components/modalPassword.jsx
+++ components/modalPassword.jsx
@@ -3,18 +3,51 @@ import { useForm } from "react-hook-form";
 export default function ModalPassword() {
   return (
     <div className="bg-oldwhite/60 p-5 rounded-md">
-      <div>
-        <p className="font-lucky text-dgreen text-3xl">
+      <div className="flex-col justify-center">
+        <p className="font-lucky text-dgreen text-3xl text-center text-wrap">
           ¿Olvidaste tu contraseña?
         </p>
+        <p className="font-mont text-black font-semibold text-sm text-wrap text-center">
+          Ingresa tu correo y cumpleaños para poder generar una nueva contraseña
+        </p>
       </div>
-      <div className="flex gap-8 flex-col">
-        <div className="grid gap-0.5">
+      <div className="flex gap-8 flex-col p-4">
+        <div className="grid gap-4">
           <div className="flex gap-2 font-bold justify-center">
-            <input type="text" name="" id="" />
+            <img src="/mail.svg" alt="" className="w-9 h-9" />
+            <input
+              type="email"
+              name=""
+              id=""
+              className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
+            />
           </div>
           <div className="flex gap-2 font-bold justify-center">
-            <input type="text" name="" id="" />
+            <img src="/icon_cumple.svg" alt="" className="w-9 h-9" />
+            <input
+              type="date"
+              name=""
+              id=""
+              className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
+            />
+          </div>
+          <div className="flex gap-2 font-bold justify-center">
+            <img src="/lock.svg" alt="" className="w-9 h-9" />
+            <input
+              type="password"
+              name=""
+              id=""
+              className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
+            />
+          </div>
+          <div className="flex gap-2 font-bold justify-center">
+            <img src="/password.svg" alt="" className="w-9 h-9" />
+            <input
+              type="text"
+              name=""
+              id=""
+              className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
+            />
           </div>
         </div>
       </div>
