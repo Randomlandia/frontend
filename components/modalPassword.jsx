c439489bec593542e7d2c6892eeeb167d80ed6da
Sonia Aguirre <arembiam@gmail.com>
2024-07-19 17:26:01 -0600

    ModalPassword structure done

--- components/modalPassword.jsx
+++ components/modalPassword.jsx
@@ -2,23 +2,26 @@ import { useForm } from "react-hook-form";
 
 export default function ModalPassword() {
   return (
-    <div className="bg-oldwhite/60 p-5 rounded-md">
+    <div className="flex flex-col bg-oldwhite/60 p-5 rounded-md">
+      {/* TITLE + SUBTITLE  */}
       <div className="flex-col justify-center">
         <p className="font-lucky text-dgreen text-3xl text-center text-wrap">
           ¿Olvidaste tu contraseña?
         </p>
-        <p className="font-mont text-black font-semibold text-sm text-wrap text-center">
-          Ingresa tu correo y cumpleaños para poder generar una nueva contraseña
+        <p className="font-mont text-black text-center">
+          Ingresa los siguientes datos
         </p>
       </div>
+
+      {/* FORMULARIO */}
       <div className="flex gap-8 flex-col p-4">
         <div className="grid gap-4">
           <div className="flex gap-2 font-bold justify-center">
             <img src="/mail.svg" alt="" className="w-9 h-9" />
             <input
               type="email"
-              name=""
-              id=""
+              name="correoRegistro"
+              placeholder="Ingresa tu correo"
               className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
             />
           </div>
@@ -26,8 +29,7 @@ export default function ModalPassword() {
             <img src="/icon_cumple.svg" alt="" className="w-9 h-9" />
             <input
               type="date"
-              name=""
-              id=""
+              name="fechaNacimiento"
               className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
             />
           </div>
@@ -35,8 +37,8 @@ export default function ModalPassword() {
             <img src="/lock.svg" alt="" className="w-9 h-9" />
             <input
               type="password"
-              name=""
-              id=""
+              name="newPassword"
+              placeholder="Nueva contraseña"
               className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
             />
           </div>
@@ -44,10 +46,20 @@ export default function ModalPassword() {
             <img src="/password.svg" alt="" className="w-9 h-9" />
             <input
               type="text"
-              name=""
-              id=""
+              name="confirm-newPassword"
+              placeholder="Repite tu contraseña"
               className="w-60 rounded-xl px-3 outline-lorange/50 outline-offset-1 shadow-md bg-lorange/70"
             />
+          </div>
+
+          {/* BTN SUBMIT  */}
+          <div className="grid justify-center gap-3">
+            <button
+              type="submit"
+              className="bg-agreen p-1.5 w-56 m-auto font-lucky text-white text-xl tracking-wider rounded-full"
+            >
+              enviar
+            </button>
           </div>
         </div>
       </div>
--- pages/pruebas.jsx
+++ pages/pruebas.jsx
@@ -3,11 +3,8 @@ import ModalPassword from "@/components/modalPassword";
 export default function Pruebas() {
   return (
     <div className=" flex min-h-screen font-mont flex-col font-bold overflow-hidden items-center bg-white">
-      {/*nav */}
-      <div className=" fixed w-full h-14 z-[4000] bg-lorange flex justify-between items-center text-white font-lucky text-xl py-3">
-        <Navbar />
-      </div>{" "}
-      <div className="p-20">
+      <Navbar />
+      <div className="m-5">
         <ModalPassword />
       </div>
     </div>
