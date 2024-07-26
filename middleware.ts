17d2f42f6ff27f21d68dc41b51148ea552c54f6e
angelagar <ggarcia.angelad@gmail.com>
2024-07-26 09:15:40 -0600

    fix: middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default withClerkMiddleware((req) => {
+  return NextResponse.next();
+});
 
 export const config = {
+<<<<<<< HEAD
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+=======
+  matcher: '/((?!_next/image|_next/static|favicon.ico).*)',
+>>>>>>> cfeb392 (fix: middleware.ts file)
 };
