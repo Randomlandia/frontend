f9bfddd656b5ac0dc30215edde890567be8e7a5a
Jesica Reyes <152235404+JesiRei333@users.noreply.github.com>
2024-08-11 15:25:39 -0600

    feat: user index animation (#121)
    Co-authored-by: Jesica Reyes <miucomosmos333@gmail.com>

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -18,18 +18,22 @@ export default function User() {
       <Navbar />
 
       {/* CARD CONTAINER */}
-      <div className=" bg-oldwhite rounded-xl p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
+      <div className=" bg-oldwhite rounded-xl blur-sm p-6 mt-10 h-4/5 w-[350px] md:w-4/5 lg:w-1/2 mx-auto">
         <div className="md:grid grid-cols-2 ">
           {/* AVATAR COMPONENT Y NOMBRE USER*/}
           <div className="p-4 px-10 lg:px-2">
-            <Avatar />
+            <img
+              src="/avatars/A_RANDY_DED.svg"
+              alt="RANDY_DED"
+              className="h-32 w-32 animate-spin"
+            />
           </div>
 
           {/* SCORE */}
           <div className="py-4 px-10 grid grid-cols-1 place-items-center lg:px-2">
             <div className="border-4 border-lorange rounded-lg py-2 px-4 grid grid-rows-2 gap-2 w-fit">
               {" "}
-              <p className="font-lucky text-dgreen text-2xl  text-center">
+              <p className="font-lucky animate-heartbeat text-dgreen text-2xl  text-center">
                 SCORE
               </p>
               <p className="font-mont font-semibold text-black text-5xl text-center ">
@@ -42,44 +46,28 @@ export default function User() {
         {/* BOTONES HACIA SUBMENUS */}
         <div className="grid gap-4 w-full py-4 px-10 md:px-32 lg:grid-cols-2 lg:gap-2 lg:px-2 xl:grid-cols-4 ">
           <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
-            <img
-              src="/icon_userheart.svg"
-              alt=""
-              className="h-8 w-8 "
-            />
+            <img src="/icon_userheart.svg" alt="" className="h-8 w-8 " />
             <span className="font-lucky text-black text-xl xl:text-lg">
               FAVS
             </span>
           </div>
 
           <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
-            <img
-              src="/icon_userachieve.svg"
-              alt=""
-              className="h-8 w-8 "
-            />
+            <img src="/icon_userachieve.svg" alt="" className="h-8 w-8 " />
             <span className="font-lucky text-black text-xl xl:text-lg">
               LOGROS
             </span>
           </div>
 
           <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
-            <img
-              src="/icon_userview.svg"
-              alt=""
-              className="h-8 w-8 "
-            />
+            <img src="/icon_userview.svg" alt="" className="h-8 w-8 " />
             <span className="font-lucky text-black text-xl xl:text-lg ">
               VISTOS
             </span>
           </div>
 
           <div className="bg-lorange rounded-lg py-2 px-4 inline-flex gap-1 place-content-evenly">
-            <img
-              src="/icon_userabt.svg"
-              alt=""
-              className="h-8 w-8"
-            />
+            <img src="/icon_userabt.svg" alt="" className="h-8 w-8" />
 
             <span className="font-lucky text-black text-xl xl:text-lg">
               NOSOTROS
@@ -89,13 +77,16 @@ export default function User() {
       </div>
 
       {/* FOOTER CONTACTO */}
+
       <ContactoFooter>
-        <SpeechBubble
-          text="Regístrate para ver más"
-          trianglePosition="rigth"
-          width="200px"
-          height="200px"
-        />
+        <div className="relative pb-11 lg:absolute lg:bottom-[90%] lg:end-[60%] [&>div>div>p]:text-natD [&>div>div>p]:font-ram [&>div>div>p]:text-xs  [&>div]:shadow-none ">
+          <SpeechBubble
+            text="Hey tú!!! registrate para ver más ♥"
+            trianglePosition="left"
+            width="200px"
+            height="150px"
+          />
+        </div>
       </ContactoFooter>
     </main>
   );
