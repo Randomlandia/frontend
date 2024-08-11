4c20634719f5bba2674e8707eb0ce75403849f40
angelagar <ggarcia.angelad@gmail.com>
2024-08-11 15:12:33 -0600

    fix: modalTest bugs to prepare for ADS

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Montserrat+Alternates:wght@400;700&family=Rammetto+One&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/RANDY_02.svg" />
+        <link rel="icon" type="image/svg" sizes="16x16" href="/RANDY_02.svg" />
       </Head>
       <body>
         <Main />
