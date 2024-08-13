bc8be9ef1d3a57c8af8a1501fe98716749b814a0
Moisés García <79472169+MoisesGJ@users.noreply.github.com>
2024-08-12 21:33:28 -0600

    feat: Added images of us from the CDN and updated the Next.js configu… (#124)
    * feat: Added images of us from the CDN and updated the Next.js configuration to accept images from our domain
    
    * feat: add randy description

--- components/constants/descrip.jsx
+++ components/constants/descrip.jsx
@@ -1,51 +1,59 @@
 import React from "react";
 export const descrip = [
   {
-    icono: "/about/iconoUser.svg",
-    alt: "iconoUser1",
+    icono: "https://cdn.randomlandia.com/people/randy.webp",
+    alt: "Randy image",
+    nombre: "Randy",
+    titulo: "CEO de Randomlandia",
+    contenido:
+      "¡Me encanta aprender! Pero aún más, ¡el enseñar! ¿Te apetecé compartir conocimiento mientras tomamos café? ¡Randomlandia me llama! Nos vemos ahí?",
+  },
+  {
+    icono: "https://cdn.randomlandia.com/people/aizza.webp",
+    alt: "Aizza image",
     nombre: "Aizza",
     titulo: "Estudiambre",
     contenido:
       "Ilustradora amateur, mega fan del ánime, amante de los conejos, obsesionada con sucesos paranormales y firmemente creyente de que Randy será el futuro vencedor de Sukuna.",
   },
   {
-    icono: "/about/iconoUser.svg",
-    alt: "iconoUser2",
+    icono: "https://cdn.randomlandia.com/people/soni.webp",
+    alt: "Sonia image",
     nombre: "Sonia Aguirre",
     titulo: "Diseñadora",
     contenido:
       "Diseñadora y Dev de tiempo parcial, catadora de memes de tiempo completo. Amante de la animación, ciencia ficción y fantasía. Me gusta investigar y desarrollar para poder crear más cosas.",
   },
   {
-    icono: "/about/iconoUser.svg",
-    alt: "iconoUser3",
+    icono: "https://cdn.randomlandia.com/people/jesi.webp",
+    alt: "Jesica image",
     nombre: "Jesica Reyes",
     titulo: "Líder",
     contenido:
       "Amante de los videojuegos, el rosa y los gatos. ¡Tú y yo ganaremos!",
   },
   {
-    icono: "/about/iconoUser.svg",
-    alt: "iconoUser4",
+    icono: "https://cdn.randomlandia.com/people/angie.webp",
+    alt: "Angie image",
     nombre: "Angie garcia",
     titulo: "todóloga software god ",
     contenido:
       "“Conectar, crear y crecer”. Madre de familia con AuTDAH. desarrolladora fullstack, arroz de todos los moles, adolescente infiltrada... ¡¡Auxilio éste cuerpo no es mio!! ",
   },
   {
-    icono: "/about/iconoUser.svg",
-    alt: "iconoUser5",
+    icono: "https://cdn.randomlandia.com/people/cris.webp",
+    alt: "Cris image",
     nombre: "cris molina",
     titulo: "software engineer",
     contenido:
       "Me gusta la música, la programación y los videojuegos (no necesariamente en ese orden). Si no estoy  jugando league o desarrollando, seguro podrás encontrarme tocando música o atrapado en el gym.",
   },
   {
-    icono: "/about/randyPerfil.svg",
-    alt: "iconoUser6",
-    nombre: "Randy",
-    titulo: "Explorador",
+    icono: "https://cdn.randomlandia.com/people/moi6.webp",
+    alt: "Moisés image",
+    nombre: "Moi6",
+    titulo: `software developer enginner ultra PRO aka "CABO"`,
     contenido:
-      "¡Me encanta aprender! Pero aún más, ¡el enseñar! ¿Te apetecé compartir conocimiento mientras tomamos café? ¡Randomlandia me llama! Nos vemos ahí?",
+      "Antiguo dueño de una startup exitosa (pero no hablen de ella porque me pongo sensible). Ahora un simple becario",
   },
 ];
--- components/landing/cardNosotrosDescri.jsx
+++ components/landing/cardNosotrosDescri.jsx
@@ -1,19 +1,23 @@
+import Image from "next/image";
 import React from "react";
+
 export default function CardNosotrosDescri(props) {
   return (
     <div className="flex pr-[120px]  md:pr-0 h-[300px]  md:bg-[#f6ead7]/75 flex-col rounded-3xl m-[5px] md:w-[250px] md:h-[320px]  gap-[10px] pt-[30px]  items-center align-middle text-center">
       <div className="flex-col ">
         <div className=" flex m-auto flex-col h-[60px] w-[60px] items-center align-middle">
-          <img
+          <Image
             src={props.icono}
             alt={props.alt}
-            className="flex w-[60px] h-[60px] align-middle items-center hover:shadow-xl hover:shadow-amber-500 hover:rounded-full "
+            width={60}
+            height={60}
+            className="flex align-middle items-center rounded-full hover:shadow-xl hover:shadow-amber-500 hover:rounded-full "
           />
         </div>
 
         <div className="flex flex-col font-lucky  text-dgreen text-[24px]">
           <div className="text-dgreen ">{props.nombre}</div>
-          <div className="flex font-lucky justify-center  text-dorange text-[16px]">
+          <div className="flex font-lucky justify-center  text-dorange text-[16px] px-3">
             {props.titulo}
           </div>
         </div>
